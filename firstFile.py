840d9cb1a27303cf29a5cc612ca24b265fb4deb4
Kartik Kommu <superracerkk@gmail.com>
2024-07-04 10:47:55 -0700

    Updated Drag, Seems to be functioning

--- firstFile.py
+++ firstFile.py
@@ -1,32 +1,47 @@
 from math import *
 from matplotlib import pyplot as plt
 
-'''
+"""
 for index in range(4,8):
     print(index)
-'''
+"""
 
 
-
-
-def projectileMotion(angle,increment,initialVel, initialH, initialAcc,flightT, dragCoeff, csa, airDensity, mass,  airResistance):
+def projectileMotion(
+    angle,
+    increment,
+    initialVel,
+    initialH,
+    initialAcc,
+    flightT,
+    dragCoeff,
+    csa,
+    airDensity,
+    mass,
+    airResistance,
+):
     time = 0
-    velocity = [0,0]
-    position = [0,0]
-    velocity[0] = initialVel*cos(angle)
-    velocity[1] = initialVel*sin(angle)
+    velocity = [0, 0]
+    position = [0, 0]
+    velocity[0] = initialVel * cos(angle)
+    velocity[1] = initialVel * sin(angle)
     position[1] = initialH
     acceleration = initialAcc
     posValX = []
     posValY = []
-    while time<flightT:
-         # potential issues here with assuming that dragCoeff is constant when it isnt
-         #also unsure if acceleration is zeroing out like it should with drag
+    while time < flightT:
+        # potential issues here with assuming that dragCoeff is constant when it isnt
+        # also unsure if acceleration is zeroing out like it should with drag
         if airResistance:
-            absVel = [abs(velocity[0]),abs(velocity[1])]
-            acceleration[0] = acceleration[0] -  ((0.5)*dragCoeff*airDensity*csa*absVel[0]*velocity[0])/mass
-            acceleration[1] = acceleration[1] - ((0.5)* dragCoeff * airDensity * csa * absVel[1] * velocity[1])/mass
-
+            absVel = [abs(velocity[0]), abs(velocity[1])]
+            acceleration[0] = (
+                -((0.5) * dragCoeff * airDensity * csa * absVel[0] * velocity[0]) / mass
+            )
+            acceleration[1] = (
+                -9.81
+                - ((0.5) * dragCoeff * airDensity * csa * absVel[1] * velocity[1])
+                / mass
+            )
 
         else:
             acceleration[0] = 0
@@ -35,9 +50,9 @@ def projectileMotion(angle,increment,initialVel, initialH, initialAcc,flightT, d
         time += increment
 
         velocity[0] = velocity[0] + increment * acceleration[0]
-        velocity[1] = velocity[1] + increment*acceleration[1]
-        position[0] = position[0] + velocity[0]*increment
-        position[1] = position[1] + increment*velocity[1]
+        velocity[1] = velocity[1] + increment * acceleration[1]
+        position[0] = position[0] + velocity[0] * increment
+        position[1] = position[1] + increment * velocity[1]
         print(position)
         posValX.append(position[0])
         posValY.append(position[1])
@@ -54,14 +69,9 @@ def projectileMotion(angle,increment,initialVel, initialH, initialAcc,flightT, d
     plt.show()
 
 
-projectileMotion(pi/4,0.1,50,30,[0,-9.81],7.98, 0,0,0, 0.8, False)
+projectileMotion(pi / 4, 0.1, 50, 30, [0, -9.81], 7.98, 0, 0, 0, 0.8, False)
 # modeling values after avg tennis ball
-projectileMotion(pi/4,0.1,50,30,[0,-9.81],7.98, 0.53,0.0154,1.225, 0.8,True)
+projectileMotion(pi / 4, 0.1, 50, 30, [0, -9.81], 7.98, 0.53, 0.0154, 1.225, 0.8, True)
 
 
 # testing
-
-
-
-
-
