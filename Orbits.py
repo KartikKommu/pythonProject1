e26050bd1f74b0079d306797ba86768146e37e17
Kartik Kommu <superracerkk@gmail.com>
2024-07-19 11:33:44 -0700

    Orbit Updates

--- Orbits.py
+++ Orbits.py
@@ -1,18 +1,40 @@
 from math import *
 from matplotlib import pyplot as plt
 import numpy as np
-
+from scipy.integrate import odeint
 
 # uses MKS Units
-def orbitEulerCromer(radius, acceleration, satMass, ancMass, increment, length):
-    t = 0
-    velocity = [0, 0]
-    position = []
-    positionVal = []
+
+G = 6.67 * (10 ^ -11)
+
+
+def orbitEulerCromer(
+    initVel, initTheta, radius, acceleration, satMass, ancMass, increment, length
+):
+    t = 0.0
+    velocity = initVel
+
+    theta = initTheta
+    thetaVal = []
     acceleration = (
         (-G * satMass * ancMass * radius) / abs(radius * radius * radius) * satMass
     )
 
     while t < length:
 
-        velocity = sqrt(G * ancMass * ((2 / radius) - (1 / acceleration)))
+        t += increment
+
+        velocity = velocity + increment * acceleration * (theta)
+        theta = theta + increment * velocity
+
+        thetaVal.append(theta)
+
+    ax = plt.subplot(111, projection="polar")
+    ax.plot(thetaVal)
+
+    plt.show()
+
+
+orbitEulerCromer(
+    12700, 0, 8.28784 * (10 ^ 11), 0.0, 5 * (10 ^ 14), 1.9891 * (10 ^ 30), 0.02, 1000
+)
