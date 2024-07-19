d48496065ae37108b0a22e022e13bbe0cff983f4
Kartik Kommu <superracerkk@gmail.com>
2024-07-18 18:13:13 -0700

    Another ODE example

--- ODE_practice.py
+++ ODE_practice.py
@@ -30,5 +30,35 @@ def system_of_odes(
 
 solution = odeint(system_of_odes, y0=N_0, t=timepoints, args=(r,))
 
-plt.plot(solution)
+plt.plot(timepoints, solution)
+plt.show()
+
+
+# Higher Order Example
+
+# 1-Constants/Initial Conditions
+
+m = 1
+b = 0.7
+k = 1
+
+init_conditions = [1.0, 0.0]
+time_points = np.linspace(0, 100, 1001)
+
+# 2-Defining System
+
+
+def system_of_odes2(y, t, m, b, k):
+
+    x, xdot = y
+
+    xddot = -b / m * xdot - k / m * x
+
+    return xdot, xddot
+
+
+solution1 = odeint(system_of_odes2, y0=init_conditions, t=time_points, args=(m, b, k))
+x_sol = solution1[:, 0]
+x_dot_sol = solution1[:, 1]
+plt.plot(time_points, solution1)
 plt.show()
