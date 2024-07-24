5ac9baaccaf509db13e9f439075f33245caf49d5
Kartik Kommu <superracerkk@gmail.com>
2024-07-23 17:25:41 -0700

    Finally Got rk4Method and EC Orbit working

--- Orbits.py
+++ Orbits.py
@@ -3,12 +3,13 @@ from matplotlib import pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 
-# uses MKS Units
+# uses AU Units
 
-G = 6.67 * (10**-11)
+G = 4 * pi**2
 
 
-def orbitEulerCromer(initVel, initRadius, satMass, ancMass, increment, length):
+# using with orbits that have any appreciable eccentricity leads to failure of method
+def orbitEulerCromer(initVel, initRadius, ancMass, increment, length):
 
     t = 0.0
     velocity = [0.0, initVel]
@@ -17,10 +18,16 @@ def orbitEulerCromer(initVel, initRadius, satMass, ancMass, increment, length):
     r = [initRadius, 0]
     ValX = []
     ValY = []
+    rVal = []
+    thetaVal = []
 
     while t < length:
 
-        acceleration[0] = -(G * ancMass) * r[0] / abs(r[0]) ** 3
+        magR = sqrt((r[0] ** 2) + (r[1] ** 2))
+
+        # fails if y starts at 0, must be a solution
+        acceleration[0] = -(G * ancMass) * r[0] / magR**3
+        acceleration[1] = -(G * ancMass) * r[1] / magR**3
 
         velocity[0] = velocity[0] + increment * acceleration[0]
         velocity[1] = velocity[1] + increment * acceleration[1]
@@ -30,10 +37,61 @@ def orbitEulerCromer(initVel, initRadius, satMass, ancMass, increment, length):
 
         ValX.append(r[0])
         ValY.append(r[1])
+        rVal.append(magR)
+        thetaVal.append(atan2(r[1], r[0]))
 
+    # currently unable to properly plot Orbital data.
+    ax = plt.subplot(111, projection="polar")
 
-
+    ax.plot(thetaVal, rVal)
     plt.show()
 
 
-orbitEulerCromer(29806.07, 1.496 * (10**11), 5 * (10**14), 1.9891 * (10**30), 0.02, 40)
+orbitEulerCromer(2 * pi, 1, 1, 0.02, 4)
+
+
+# where f(x,t) is the right hand side of a DE dx/dt = f(x,t)
+# initConditions = [x,t]
+def rk4Method(function, increment, tMax, initConditions):
+    xData = []
+    tData = []
+    x = initConditions[0]
+    t = initConditions[1]
+    while t < tMax:
+        F1 = function(x, t)
+        F2 = function(x + ((increment / 2) * F1), t + (increment / 2))
+        F3 = function(x + ((increment / 2) * F2), t + (increment / 2))
+        F4 = function(x + increment * F3, t + increment)
+        t = t + increment
+
+        x = x + ((1 / 6) * increment * (F1 + 2 * F2 + 2 * F3 + F4))
+        xData.append(x)
+        tData.append(t)
+    return xData, tData
+
+
+def fun(x, t):
+
+    fun = 3 * t**2
+    return fun
+
+
+x, t = rk4Method(fun, 0.1, 2, [-8, -2])
+plt.plot(t, x)
+plt.show()
+
+
+def orbitRK4(initVel, initRadius, satMass, ancMass, increment, length):
+
+    t = 0.0
+    t = 0.0
+    velocity = [0.0, initVel]
+    acceleration = [0.0, 0.0]
+
+    r = [initRadius, 1]
+    ValX = []
+    ValY = []
+
+    while t < length:
+        acceleration[0] = -(G * ancMass) * r[0] / abs(r[0]) ** 3
+        acceleration[1] = -(G * ancMass) * r[1] / abs(r[1]) ** 3
