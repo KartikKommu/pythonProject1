9edfa164ab8587e77940c4f0567d47c822958a47
Kartik Kommu <superracerkk@gmail.com>
2025-07-21 10:48:51 -0700

    new file

--- numpyPractice.py
+++ numpyPractice.py
@@ -49,6 +49,46 @@ y = 1 / x**2 * np.sin(x)
 dydx = np.gradient(y, x)
 y_int = np.cumsum(y) * (x[1] - x[0])
 
+# multiD-arrays
+
+a1 = np.array([[1, 4, 5], [4, 5, 6], [7, 1, 9]])
+
+print(a1 > 3)
+a1[0]
+print(a1[:, 1])
+print(a1[1:, 2])
+print(a1[1:, :2])
+
+x = np.linspace(0, 10, 1000)
+y = np.linspace(0, 10, 1000)
+xv, yv = np.meshgrid(
+    x, y
+)  ##create a 2d array(xv and yv) for x and y for multi-variable fxn
+
+zv = xv**2 + yv**2
+
+# plt.contourf(xv, yv, zv, levels=50)
+# plt.colorbar()
+
+# plt.show()
+
+# basic lin-Alg
+A = np.array([[3, 2, 1], [5, -5, 4], [6, 0, 1]])
+b1 = np.array([1, 2, 3])
+b2 = np.array([-1, 2, -5])
+c = np.array([4, 0, 3])
+
+print(np.linalg.solve(A, c))
+
+
+print(A @ b1)
+A.T
+
+np.dot(b1, b2)
+np.cross(b1, b2)
+
+D = np.array([[4, 2, 2], [2, 4, 2], [2, 2, 4]])
+print(np.linalg.eig(D))  ##eigenvectors are the columns of the 2d array, not rows
 
 # examples
 
@@ -63,7 +103,7 @@ print(np.mean(y[(x >= 4) * (7 >= x)]))
 print(x[1:][0 > ((dydx[1:]) * (dydx[:-1]))])
 
 
-plt.show()
+# plt.show()
 
 x = np.arange(0, 10001, 1)
 
@@ -78,12 +118,28 @@ drdt = np.gradient(radius, theta)
 x = radius * np.cos(theta)
 y = radius * np.sin(theta)
 
-area = np.cumsum(radius) * (theta[1] - theta[0])
-arclength = np.cumsum(np.sqrt((radius**2) + (drdt**2))) * (theta[1] - theta[0])
+area = np.sum(radius) * (theta[1] - theta[0])
+arclength = np.sum(np.sqrt((radius**2) + (drdt**2))) * (theta[1] - theta[0])
 
 print(arclength)
 
 ##plt.plot(theta, area)
 # plt.plot(theta, arclength)
 # plt.plot(x, y)
+# plt.show()
+
+
+x = np.linspace(-2, 2, 1000)
+y = np.linspace(-2, 2, 1000)
+
+xv, yv = np.meshgrid(x, y)
+zv = np.exp(-((xv**2) + (yv**2))) * np.sin(xv)
+
+print(np.sum(np.abs(zv)) * (x[1] - x[0]) * (y[1] - y[0]))
+print(
+    np.sum(np.abs(zv[np.sqrt((xv**2 + yv**2)) > 0.5])) * (x[1] - x[0]) * (y[1] - y[0])
+)
+
+# plt.contourf(xv, yv, zv, levels=40)
+# plt.colorbar()
 # plt.show()
