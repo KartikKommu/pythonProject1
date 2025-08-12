7a67ac12bdd2a412a4ae6bf297303a613ea42582
kartik@macbook <superracerkk@gmail.com>
2025-08-12 16:26:18 -0700

    reworked double pendulum with the use of symbolic methods, making much more progress`

--- doublePendulum.py
+++ doublePendulum.py
@@ -1,7 +1,7 @@
 import numpy as np
+import sympy as smp
 from scipy.integrate import odeint
 from matplotlib import pyplot as plt
-from sympy import smp
 from matplotlib import animation
 from matplotlib.animation import PillowWriter
 from mpl_toolkits.mplot3d import Axes3D
@@ -13,44 +13,45 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 
+def doublePendulum(l1,l2,m1,m2):
 
-def doublePendulum1(m1, theta1in, length1, m2, theta2in, length2, time, increment):
+    th1, th2 = smp.symbols('th1 th2', cls=smp.Function)
+    t = smp.symbols('t')
+    th1 = th1(t)
+    th2 = th2(t)
 
-    ##theta1 = np.linspace(0,2*np.pi,1000)
-    ##theta2 = np.linspace(0,2*np.pi,1000)
-    ##theta1[0] = theta1in
-    ##theta2[0] = theta2in##
+    x1 = l1*smp.sin(th1)
+    y1 = -l1*smp.cos(th1)
+    x2 = l1*smp.sin(th1) + l2*smp.sin(th2)
+    y2 = -l1*smp.cos(th1) + l2*smp.cos(th2)
 
-    tval = np.linspace(0, time, int(time/increment))
-    x1val = np.linspace(0,)
-    x2val = np.linspace(0,)
-    y1val = np.linspace(0,)
-    y2val = np.linspace(0,)
+    x1dot = smp.diff(x1,t)
+    y1dot = smp.diff(y1, t)
+    x2dot = smp.diff(x2,t)
+    y2dot = smp.diff(y2,t)
 
+    T = smp.Rational(1,2)*m1*(y1dot**2+x1dot**2) + smp.Rational(1,2)*m2*(y2dot**2+x2dot**2)
+    U = m1*9.81*y1 + m2*9.81*y2
 
+    L = T - U
 
-    x1 = length1 * np.sin(theta1in)
-    y1 = -length1 * np.cos(theta1in)
-    x2 = length1 * np.sin(theta1in) + length2 * np.sin(theta2in)
-    y2 = -length1 * np.cos(theta1in) + length2 * np.cos(theta2in)
-
-    vx1 = length1 * np.sin
-
-
-
-    counter = 0
-
-    while t<time:
-
-        x1[counter] = length1 * np.sin(theta1)
-        y1[counter] = length1 * np.cos(theta1)
-        x2 = length1 * np.sin(theta1) + length2 * np.sin(theta2)
-        y2 = length1 * np.cos(theta1) + length2 * np.cos(theta2)
+    eq1 = smp.diff(smp.diff(L,smp.diff(th1,t)),t) - smp.diff(L,th1)
+    eq2 = smp.diff(smp.diff(L,smp.diff(th2,t)),t) - smp.diff(L,th2)
 
 
 
 
 
-        counter += increment
+
+    print(L)
+
+
+
+
+
+doublePendulum(3,4,5,5)
+
+
+
 
 
