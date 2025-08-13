7d55487f2c2eb984ff6b44bdbd5cb6013406ddbb
kartik@macbook <superracerkk@gmail.com>
2025-08-13 15:02:53 -0700

    updates to double pendulum, mostly finished the numerical caluclation section, issues with proper calculating, may have something to do with using variables rather than symbols for m1 m2 l1 l2. still need to start and finish animation. want to take care of calculational issue first

--- doublePendulum.py
+++ doublePendulum.py
@@ -5,20 +5,20 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 from matplotlib.animation import PillowWriter
 from mpl_toolkits.mplot3d import Axes3D
+from sympy import false
 
 
-
-
-
-
-
-
-def doublePendulum(l1,l2,m1,m2):
+def doublePendulum(l1,l2,m1,m2,t_total,interval,i_th1,i_z1,i_th2,i_z2):
 
     th1, th2 = smp.symbols('th1 th2', cls=smp.Function)
     t = smp.symbols('t')
     th1 = th1(t)
     th2 = th2(t)
+
+    th1dot = smp.diff(th1, t)
+    th2dot = smp.diff(th2, t)
+    th1dd = smp.diff(th1dot, t)
+    th2dd = smp.diff(th2dot, t)
 
     x1 = l1*smp.sin(th1)
     y1 = -l1*smp.cos(th1)
@@ -35,21 +35,45 @@ def doublePendulum(l1,l2,m1,m2):
 
     L = T - U
 
-    eq1 = smp.diff(smp.diff(L,smp.diff(th1,t)),t) - smp.diff(L,th1)
-    eq2 = smp.diff(smp.diff(L,smp.diff(th2,t)),t) - smp.diff(L,th2)
+    eq1 = smp.diff(smp.diff(L,th1dot),t) - smp.diff(L,th1)
+    eq2 = smp.diff(smp.diff(L,th2dot),t) - smp.diff(L,th2)
+
+    sols = smp.solve([eq1,eq2], (th1dd,th2dd), rational=false, simplify=False)
+
+    ##transition/conversion of symbolic expressions to numerical in order to solve the ODE
+
+    dz1dt = smp.lambdify((t,th1,th1dot,th2,th2dot), sols[th1dd])
+    dz2dt = smp.lambdify((t,th1,th1dot,th2,th2dot), sols[th2dd])
+    dth1dt = smp.lambdify(th1dot, th1dot)
+    dth2dt = smp.lambdify(th2dot, th2dot)
+
+    ##then we solve the system of DE's above
+
+    def dSdt(S,t):
+        th1, z1, th2, z2 = S
+
+        return [
+            dth1dt(z1),
+            dz1dt(t, th1,th2,z1,z2),
+            dth2dt(z2),
+            dz2dt(t,th1,th2,z1,z2),
+        ]
+
+    t = np.arange(0, t_total, interval)
+    ans = odeint(dSdt, y0=[i_th1,i_z1,i_th2,i_z2], t = t)
+
+    plt.plot(t,ans.T[2])
+    plt.show()
 
 
 
 
 
 
-    print(L)
 
 
 
-
-
-doublePendulum(3,4,5,5)
+doublePendulum(3,5,2,3,40,0.01,1, -3, -1,5)
 
 
 
